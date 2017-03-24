3614f9d9547f85caa1e58dd29b8f3742f74233f1
Win Sandy <winsandymyint@gmail.com>
2017-03-24 14:50:57 +0800

    Remove console.log

--- app/js/map.js
+++ app/js/map.js
@@ -9,7 +9,6 @@ function initMap() {
         zoom: 13,
         styles: [{"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c8d7d4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}]
     });
-    console.log(window.innerHeight);
     var windowHeight = window.innerHeight;
     var search_box = document.getElementsByClassName('map-search');
     var searchBarHeight = document.getElementById('sticker').offsetHeight;
@@ -17,7 +16,6 @@ function initMap() {
     var map_container = document.getElementById('map-top');
     var map_container_height = windowHeight - searchBarHeight - stickyHeaderHeight; 
     map_container.style.height = map_container_height+"px";
-    console.log('awefkjwe')
     new AutocompleteDirectionsHandler(map);
 }
 
@@ -25,7 +23,6 @@ function initMap() {
  * @constructor
  */
 function AutocompleteDirectionsHandler(map) {
-    console.log("WEJFKJWEKLJFKLJEFLK")
     this.map = map;
     this.originPlaceId = null;
     this.destinationPlaceId = null;
